7bc15f5b54059dcdeae97c25dbe49e32e89f4f17
Mikhail Kormyshov <kormyshov@gmail.com>
2014-06-17 23:04:05 +0800

    Add delete school

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <sqlite3.h>
+#include <vector>
 #include <map>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -10,7 +12,10 @@ void print_menu();
 void menu_exit();
 void menu_create_bd();
 void menu_add_school();
+void menu_del_school();
 void menu_print_schools();
+
+bool is_similary_school(const char *, const char *);
 
 string fname = "db.sqlite3";
 sqlite3 *db = 0;
@@ -26,6 +31,7 @@ int main(){
 	map<int, void (*)()> func = {
 									{101, menu_create_bd},
 									{102, menu_add_school},
+									{103, menu_del_school},
 									{104, menu_print_schools},
 									{0,   menu_exit}
 	};
@@ -47,6 +53,7 @@ void print_menu(){
 	cout << "\n\n\nДобро пожаловать в PectiBD.\n\nГлавное меню:\n";
 	cout << "\n101. Создание базы данных\n";
 	cout << "102. Добавить школу\n";
+	cout << "103. Удалить школу\n";
 	cout << "104. Вывести школы\n";
 	cout << "0. Выход\n\n";
 	cout << "Введите номер пункта меню: ";
@@ -72,7 +79,22 @@ void menu_create_bd(){
 		return ;
 	}
 
+	ret = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Courses(Name, Dat_name)", 0, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при создании таблицы Courses: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
 
+}
+
+vector<string> school_names;
+int add_school_name(void *not_used, int cnt, char **arg, char **sz){
+
+	string s(arg[0]);
+	school_names.push_back(s);
+
+	return 0;
 }
 
 void menu_add_school(){
@@ -82,12 +104,30 @@ void menu_add_school(){
 	cin.getline(name, 100);
 	cin.getline(name, 100);
 
+	char *err = 0;
+	int ret = 0;
+
+	school_names.clear();
+	ret = sqlite3_exec(db, "SELECT Name FROM Schools ORDER BY Name", add_school_name, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при считывании из таблицы Schools: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+	cout << "\nВозможно Вы имели ввиду одну из этих школ:\n";
+	for(int i=0; i<(int)school_names.size(); ++i){
+		if(is_similary_school(name, school_names[i].c_str())) cout << school_names[i] << endl;
+	}
+	cout << "Прервать добавление новой школы? (y/n): ";
+	char c;
+	cin >> c;
+	if(c == 'y') return ;
+
 	cout << "Введите название школы в родительном падеже (например Гимназии №11 г. Красноярска или Шуваевской СОШ): ";
 	char gen_name[101];
 	cin.getline(gen_name, 100);
-
-	char *err = 0;
-	int ret = 0;
+	cin.getline(gen_name, 100);
 
 	char query[1001];
 	sprintf(query, "INSERT INTO Schools VALUES (\"%s\", \"%s\")", name, gen_name);
@@ -101,7 +141,49 @@ void menu_add_school(){
 
 }
 
-void print_string(char *s, int cnt){
+void menu_del_school(){
+
+	cout << "Введите название школы (например Гимназия №11 г. Красноярска или Шуваевская СОШ): ";
+	char name[101];
+	cin.getline(name, 100);
+	cin.getline(name, 100);
+
+	char *err = 0;
+	int ret = 0;
+
+	school_names.clear();
+	ret = sqlite3_exec(db, "SELECT Name FROM Schools ORDER BY Name", add_school_name, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при считывании из таблицы Schools: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+	cout << "\nВозможно Вы имели ввиду одну из этих школ:\n";
+	int cnt = 1;
+	vector<string> similary_sch;
+	for(int i=0; i<(int)school_names.size(); ++i){
+		if(is_similary_school(name, school_names[i].c_str())){
+			cout << cnt++ << ". " << school_names[i] << endl;
+			similary_sch.push_back(school_names[i]);
+		}
+	}
+	cout << "Введите номер пункта или 0, чтобы отменить: ";
+	int c;
+	cin >> c;
+	if(!c || c >= cnt) return ;
+
+	char query[1001];
+	sprintf(query, "DELETE FROM Schools WHERE Name LIKE \"%s\"", similary_sch[c-1].c_str());
+	ret = sqlite3_exec(db, query, 0, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при удалении из таблицы Schools: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+}
+
+void print_string(const char *s, int cnt){
 
 	int i;
 	for(i=0; s[i] && i<cnt; ++i){
@@ -114,8 +196,6 @@ void print_string(char *s, int cnt){
 
 int print_schools(void *not_used, int cnt, char **arg, char **sz){
 
-	//arg[0][62] = '\0';
-	//arg[1][62] = '\0';
 	printf("|| ");
 	print_string(arg[0], 30);
 	printf(" || ");
@@ -147,3 +227,38 @@ void menu_print_schools(){
 	}
 
 }
+
+int DMcost(char c1, char c2){
+
+	if(c1 == c2) return 0;
+	if((c1 >= '0' && c1 <= '9') || (c2 >= '0' && c2 <= '9')) return 5;
+	return 1;
+}
+
+int DMdistance(const char *s1, const char *s2){
+
+	int n = strlen(s1);
+	int m = strlen(s2);
+
+	int **mm = new int*[n+1];
+	for(int i=0; i<=n; ++i){
+		mm[i] = new int[m+1];
+		for(int j=0; j<=m; ++j){
+			if(!i || !j){
+				mm[i][j] = i+j;
+				continue ;
+			}
+			mm[i][j] = mm[i-1][j] + 2;
+			mm[i][j] = min(mm[i][j], mm[i][j-1] + 2);
+			mm[i][j] = min(mm[i][j], mm[i-1][j-1] + DMcost(s1[i-1], s2[j-1]));
+		}
+	}
+	return mm[n][m];
+}
+
+bool is_similary_school(const char *s1, const char *s2){
+
+	//cout << s1 << "(" << strlen(s1) << ")" << " ~ " << s2 << "(" << strlen(s2) << ")" << " = " << DMdistance(s1, s2) << endl;
+	return (DMdistance(s1, s2) < 10);
+}
+
