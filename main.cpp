373cf86adb8a6cd6bd2191140cca960424421069
Mikhail Kormyshov <kormyshov@gmail.com>
2014-06-18 00:05:36 +0800

    Add table Names with functions

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ void menu_print_schools();
 void menu_add_course();
 void menu_del_course();
 void menu_print_courses();
+void menu_add_name();
+void menu_del_name();
+void menu_print_names();
 
 bool is_similary_school(const char *, const char *);
 
@@ -39,6 +42,9 @@ int main(){
 									{105, menu_add_course},
 									{106, menu_del_course},
 									{107, menu_print_courses},
+									{111, menu_add_name},
+									{112, menu_del_name},
+									{113, menu_print_names},
 									{0,   menu_exit}
 	};
 
@@ -64,6 +70,9 @@ void print_menu(){
 	cout << "105. Добавить направление\n";
 	cout << "106. Удалить направление\n";
 	cout << "107. Вывести направления\n";
+	cout << "111. Добавить имя\n";
+	cout << "112. Удалить имя\n";
+	cout << "113. Вывести имена\n";
 	cout << "0. Выход\n\n";
 	cout << "Введите номер пункта меню: ";
 }
@@ -91,6 +100,13 @@ void menu_create_bd(){
 	ret = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Courses(Name, Dat_name)", 0, 0, &err);
 	if(ret != SQLITE_OK){
 		cout << "Ошибка при создании таблицы Courses: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+	ret = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Names(Name, Dat_name)", 0, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при создании таблицы Names: " << err << endl;
 		sqlite3_free(err);
 		return ;
 	}
@@ -346,6 +362,141 @@ void menu_print_courses(){
 	int ret = sqlite3_exec(db, "SELECT * FROM Courses ORDER BY Name", print_courses, 0, &err);
 	if(ret != SQLITE_OK){
 		cout << "Ошибка при выводе таблицы Courses: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+}
+
+bool is_similary_name(const char *s1, const char *s2){
+
+	//cout << s1 << "(" << strlen(s1) << ")" << " ~ " << s2 << "(" << strlen(s2) << ")" << " = " << DMdistance(s1, s2) << endl;
+	return (DMdistance(s1, s2) < 5);
+}
+
+vector<string> list_names;
+int add_list_name(void *not_used, int cnt, char **arg, char **sz){
+
+	string s(arg[0]);
+	list_names.push_back(s);
+
+	return 0;
+}
+
+void menu_add_name(){
+
+	cout << "Введите имя (например Иван): ";
+	char name[101];
+	cin.getline(name, 100);
+	cin.getline(name, 100);
+
+	char *err = 0;
+	int ret = 0;
+
+	list_names.clear();
+	ret = sqlite3_exec(db, "SELECT Name FROM Names ORDER BY Name", add_list_name, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при считывании из таблицы Names: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+	cout << "\nВозможно Вы имели ввиду одно из этих имён:\n";
+	for(int i=0; i<(int)list_names.size(); ++i){
+		if(is_similary_name(name, list_names[i].c_str())) cout << list_names[i] << endl;
+	}
+	cout << "Прервать добавление нового имени? (y/n): ";
+	char c;
+	cin >> c;
+	if(c == 'y') return ;
+
+	cout << "Введите имя в дательном падеже (например Ивану): ";
+	char dat_name[101];
+	cin.getline(dat_name, 100);
+	cin.getline(dat_name, 100);
+
+	char query[1001];
+	sprintf(query, "INSERT INTO Names VALUES (\"%s\", \"%s\")", name, dat_name);
+
+	ret = sqlite3_exec(db, query, 0, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при добавлении в таблицу Names: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+}
+
+void menu_del_name(){
+
+	cout << "Введите имя (например Иван): ";
+	char name[101];
+	cin.getline(name, 100);
+	cin.getline(name, 100);
+
+	char *err = 0;
+	int ret = 0;
+
+	list_names.clear();
+	ret = sqlite3_exec(db, "SELECT Name FROM Names ORDER BY Name", add_list_name, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при считывании из таблицы Names: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+
+	cout << "\nВозможно Вы имели ввиду одно из этих имён:\n";
+	int cnt = 1;
+	vector<string> similary_nm;
+	for(int i=0; i<(int)list_names.size(); ++i){
+		if(is_similary_name(name, list_names[i].c_str())){
+			cout << cnt++ << ". " << list_names[i] << endl;
+			similary_nm.push_back(list_names[i]);
+		}
+	}
+	cout << "Введите номер пункта или 0, чтобы отменить: ";
+	int c;
+	cin >> c;
+	if(!c || c >= cnt) return ;
+
+	char query[1001];
+	sprintf(query, "DELETE FROM Names WHERE Name LIKE \"%s\"", similary_nm[c-1].c_str());
+	ret = sqlite3_exec(db, query, 0, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при удалении из таблицы Names: " << err << endl;
+		sqlite3_free(err);
+		return ;
+	}
+}
+
+int print_names(void *not_used, int cnt, char **arg, char **sz){
+
+	printf("|| ");
+	print_string(arg[0], 30);
+	printf(" || ");
+	print_string(arg[1], 30);
+	printf(" ||\n");
+
+	return 0;
+}
+
+void menu_print_names(){
+
+	printf("|| ");
+	print_string("Имя", 30);
+	printf(" || ");
+	print_string("Дательный падеж", 30);
+	printf(" ||\n");
+	printf("===");
+	printf("==============================");
+	printf("====");
+	printf("==============================");
+	printf("===\n");
+
+	char *err = 0;
+	int ret = sqlite3_exec(db, "SELECT * FROM Names ORDER BY Name", print_names, 0, &err);
+	if(ret != SQLITE_OK){
+		cout << "Ошибка при выводе таблицы Names: " << err << endl;
 		sqlite3_free(err);
 		return ;
 	}
